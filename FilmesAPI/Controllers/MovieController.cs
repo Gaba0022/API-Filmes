be0791ea9612371ac4e8b8669f166b4ea10533f1
Gabriel Martinho <gabrielmartinho02@gmail.com>
2025-05-12 19:32:31 -0300

    Primeiro commit

---
+++ FilmesAPI/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieController.Controllers;
+
+
+[ApiController]
+[Route("[Controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMove([FromBody] Movie _movie)
+    {
+        movies.Add(_movie);
+        Console.WriteLine(_movie.Title);
+        Console.WriteLine(_movie.Duration);
+    }
+}
+
